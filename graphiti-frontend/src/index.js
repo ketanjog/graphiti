7dfa7f7e0fa169956b00c2c84a8c81adb31cefec
Jay Ram <jaycram@gmail.com>
2022-04-02 16:53:17 -0400

    made it a MPA

--- graphiti-frontend/src/index.js
+++ graphiti-frontend/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<App />,
   document.getElementById('root')
 );
 
